bf92b65cafd3e8584c57bb17394e7a0e5dcf3ca7
Andrey Tikhonov <65923716+mastertixon83@users.noreply.github.com>
2023-04-13 23:56:50 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
 
-
+// Привет как твои дела
 
 function App() {
 
